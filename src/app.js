29b89717fd3a3e5501834a30c69fd48409cd2f80
Sean-Veal <seand.veal@yahoo.com>
2018-02-07 14:29:25 -0600

    Getting variables from the url

--- src/app.js
+++ src/app.js
@@ -1,43 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.less';
 
-const expenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
-
-const addExpensePage = () => (
-    <div>
-        This is from my add Expense component
-    </div>
-);
-
-const editExpensePage = () => (
-    <div>
-        This is from my edit Expense component
-    </div>
-);
-
-const helpExpensePage = () => (
-    <div>
-        This is from my help Expense component
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-    <div>
-    <Route path="/" component={expenseDashboardPage} exact={true} />
-    <Route path="/create" component={addExpensePage} />
-    <Route path="/edit" component={editExpensePage} />
-    <Route path="/help" component={helpExpensePage} />
-    </div>
-
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
