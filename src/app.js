46f9c1df8aa45171b6d6df0c76b5d85923270c1b
Sean-Veal <seand.veal@yahoo.com>
2018-02-08 15:54:12 -0600

    Squashed to 1 commit
    changing states
    
    Writing Tests
    
    Writing more tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routes/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.less';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water Bill', amount: 500, createdAt: 200}));
+store.dispatch(addExpense({description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 109500, createdAt: 500}));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}> 
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/playground/hoc.js
@@ -0,0 +1,45 @@
+/* Higher order component (HOC) -
+ A component (HOC) that renders another component */
+/**
+ * HOC
+ * Reuse code
+ * Render hijacking
+ * Prop manipulationn
+ * Abstract state
+ */
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+
+ const Info = (props) => (
+     <div>
+        <h1>Info</h1>
+        <p>The info is: {props.info}</p>
+     </div>
+ );
+
+ const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            { isAdmin && <p>This is private info...please don't share</p>}
+            <WrappedComponent {...props}/>
+        </div>  
+    );
+ };
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin ? (
+                <WrappedComponent {...props}/>
+             ) : (
+                <p>Please login to view the info</p>
+             )}
+        </div>
+    );
+};
+
+ const AdminInfo = withAdminWarning(Info)
+ const AuthInfo = requireAuthentication(Info);
+ 
+
+ ReactDOM.render(<AuthInfo isAdmin={false} info="These are the details" />, document.getElementById('app'));
